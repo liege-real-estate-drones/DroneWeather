c9fe117c2d37a0316dbb6c1ee01cba9c2cfc9b22
Thibault Wislez <thibault1wislez@gmail.com>
2025-05-29 17:58:48 +0000

    leaflet

---
+++ src/types/images.d.ts
@@ -0,0 +1,21 @@
+// src/types/images.d.ts
+declare module '*.png' {
+    const value: any;
+    export default value;
+  }
+  
+  declare module '*.svg' {
+    const value: any;
+    export default value;
+  }
+  
+  declare module '*.jpeg' {
+    const value: any;
+    export default value;
+  }
+  
+  declare module '*.jpg' {
+    const value: any;
+    export default value;
+  }
+  
